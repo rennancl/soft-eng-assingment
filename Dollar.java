d7227b3953d8f5b6a31b97d17ec83a8bf78655bb
Rennan Cordeiro <rennancl@gmail.com>
2019-11-03 18:52:18 -0300

    Commit #2

--- Dollar.java
+++ Dollar.java
@@ -1,5 +1,9 @@
-class Dollar {
-        int amount = 10;
-        Dollar(int amount) {}			
-        void times(int multiplier) {}
+class Dollar  extends Money{
+        int amount;
+        Dollar(int amount) {
+                this.amount= amount;
+        }
+        void times(int multiplier) {
+                amount= amount * multiplier;
+}
 }
--- Tests.java
+++ Tests.java
@@ -3,10 +3,27 @@ import static org.junit.Assert.assertEquals;
 
 public class TestClass {
 
-    @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
-} 
+
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));   
+    }
+
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
