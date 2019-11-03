f7c48e9f11c0148f5ee2f2388b10501b9b2d3127
Rennan Cordeiro <rennancl@gmail.com>
2019-11-03 19:06:00 -0300

    Commit #5

--- Money.java
+++ Money.java
@@ -1,18 +1,29 @@
-abstract class Money {
+class Money {
    protected int amount;
-   private String currency; 
-   abstract Money times(int multiplier);
-
+   protected String currency; 
    static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
+      return new Money(amount, "USD");
    }
-
+	
    static Money franc(int amount) {
-      return new Franc(amount, "CHF");
+      return new Money(amount, "CHF");
    }
-
+    
    Money(int amount, String currency) {
       this.amount = amount;
       this.currency = currency;
+   }
+	
+   public boolean equals(Object object) {
+      Money money = (Money) object;
+      return amount == money.amount && currency().equals(money.currency());
+   }
+	
+   Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
+   }
+
+   String currency() {
+      return currency;
    }
 }
